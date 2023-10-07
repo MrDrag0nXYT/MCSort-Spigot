bc5a79554629be8c8280a9168d6589c8e5be78c9
MrDrag0nXYT <mrdrag0nxyt@gmail.com>
2023-10-07 17:25:52 +0300

    Initial Commit

---
+++ src/main/java/zxc/mrdrag0nxyt/MCSort_Spigot.java
@@ -0,0 +1,17 @@
+package zxc.mrdrag0nxyt;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MCSort_Spigot extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
